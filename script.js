6453a7ea2d682008940f486d0275a7cb3ad50c90
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-08-13 21:01:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
